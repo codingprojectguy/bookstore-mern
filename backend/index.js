bd20a3674ab14847c70aa74b5c6118d7a0403bca
Home PC <vfxprojectguy@gmail.com>
2024-04-09 10:52:48 -0700

    connect app to the mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import { mongoose } from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  console.log("Hello World!");
+  return res.status(234).send("Welcome to MERN Stack Bookstore");
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("app connect to the database");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
