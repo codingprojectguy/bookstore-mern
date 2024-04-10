606431b76ff0f0137b6034476c7dded3ec94211d
Home PC <vfxprojectguy@gmail.com>
2024-04-09 17:21:27 -0700

    get one book

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,23 @@ app.get("/books", async (req, res) => {
   try {
     const books = await Book.find({});
 
-    return res.status(200).json(books);
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+//Route for get one book from database by id
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+
+    return res.status(200).json(book);
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: error.message });
