b6413a4d4c36712aa99eada3874d4fbbfbdb1f79
Home PC <vfxprojectguy@gmail.com>
2024-04-09 18:20:01 -0700

    delete a book

--- backend/index.js
+++ backend/index.js
@@ -66,29 +66,38 @@ app.get("/books/:id", async (req, res) => {
 //route for update a book
 app.put("/books/:id", async (req, res) => {
   try {
-    // Check if all required fields are present in the request body
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
       return res.status(400).send({
-        message: "Send all required fields: title, author, publishYear",
+        message: "Send all requied fields: title, author, publishYear",
       });
     }
-
-    // Extract the book ID from the request parameters
     const { id } = req.params;
-
-    // Use Mongoose's findByIdAndUpdate to update the book
     const result = await Book.findByIdAndUpdate(id, req.body);
-
-    // If no book was found with the provided ID, return a 404 error
     if (!result) {
       return res.status(404).json({ message: "Book not found" });
     }
 
-    // If the book was successfully updated, return a success message
     return res.status(200).send({ message: "Book updated successfully" });
   } catch (error) {
-    // If an error occurs during the update process, handle it here
-    // (Currently, the catch block is empty, so errors will not be properly handled)
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+//Route for delete a book
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    return res.status(200).send({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
   }
 });
 
