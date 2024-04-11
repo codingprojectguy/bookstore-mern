4029851d501ebe29066b5646c87aa50109843124
Home PC <vfxprojectguy@gmail.com>
2024-04-10 18:22:45 -0700

    change backend link

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ export const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:5555/books")
+      .get("https://bookstore-mern-as9y.onrender.com/books")
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`https://bookstore-mern-as9y.onrender.com/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
