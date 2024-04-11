ca572f28f2dceddef1293544be65b21a3d55f8cf
Home PC <vfxprojectguy@gmail.com>
2024-04-10 18:54:47 -0700

    change port

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const PORT = `https://bookstore-mern-as9y.onrender.com`;
 
 export const mongoDBURL =
   "mongodb+srv://vfxprojectguy:jtH3aeb7dj0L7qTw@book-store-mern.98nlkyv.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN";
